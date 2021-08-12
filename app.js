37716c3d5e14c9c1091a381c51252b3f6e98842f
Autumn Raine <autumn@inerdgirl.com>
2021-08-12 10:28:34 -0600

    Added functionality for serving static pages

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const morgan = require('morgan');
 const app = express();
 
 app.use(morgan('dev'));
+app.use(express.static('public'));
+
+app.use(express.urlencoded());
 
 const PORT = 8080;
 
@@ -10,5 +13,5 @@ app.get('/', (req, res) => {
     res.send("Hello World")
 })
 
-app.listen(PORT);
+app.listen(PORT, () => console.log(`Listening on PORT:${PORT}`));
 
